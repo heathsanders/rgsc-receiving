8062df253446f4b2237c03a2199ec78fcc1b9ab4
heathsanders <sandersheath@gmail.com>
2018-02-12 19:53:21 -0700

    Million

---
+++ src/pages/r-gscreceiving/r-gscreceiving.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-r-gscreceiving',
+  templateUrl: 'r-gscreceiving.html'
+})
+export class RGSCReceivingPage {
+
+  constructor(public navCtrl: NavController) {
+  }
+  
+}
